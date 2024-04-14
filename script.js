2db7fc3d4bc31b6ced009063097929553ef01e93
shaheen <shaheen080102@gmail.com>
2024-04-14 13:03:04 +0530

    new changes

---
+++ script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.navbar button');
+const navMenu = document.querySelector('.navbar .nav-menu');
+
+function openMenu(){
+    navMenu.classList.toggle('active');
+
+    if(navMenu.classList.contains('active')){
+        hamburger.innerHTML = `<img src="img/menu.png" alt="">`;
+
+    }else{
+        hamburger.innerHTML = `<img src="img/close.png" alt="">`
+    }
+
+
+}
